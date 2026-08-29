c095e47b2c22e121b65f9821a6719d974203aca9
Yuvraj Johari <74721790+yuvrajjohari052@users.noreply.github.com>
2026-08-29 23:51:04 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -41,7 +41,7 @@ module "vpc" {
 # ---- EKS Cluster (Auto Mode) ----
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "~> 20.31"
+  version = "~> 21.0"
 
   cluster_name    = var.cluster_name
   cluster_version = var.cluster_version
